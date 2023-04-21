635c232a48c548f95668e970c3d4a81291bc08ef
32KZ <72939329+32KZ@users.noreply.github.com>
2023-04-21 10:34:52 +0100

    Continue

---
+++ Ronaldo.bat
@@ -0,0 +1,11 @@
+@echo off 
+:start 
+echo Christiano Ronaldoooooooooooooooo
+echo Christiano Ronaldoooooooooooooo
+echo Christiano Ronaldoooooooooooo
+echo Christiano Ronaldoooooooooo
+echo Christiano Ronaldooooooo
+echo Christiano Ronaldoooooooooo
+echo Christiano Ronaldoooooooooooo
+echo Christiano Ronaldoooooooooooooooo
+GOTO start: 
---
+++ Terminal.bat
@@ -0,0 +1,4 @@
+
+@echo on
+CMD.exe /k "title Terminal & prompt : & cls"
+pause
