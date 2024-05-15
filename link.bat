0039a4506de978d2eeb0fdad9d63f79a17e33065
32KZ <dandogdin@gmail.com>
2024-05-15 14:22:55 +0100

    Update Link.bat
    simpler code

--- link.bat
+++ link.bat
@@ -1,10 +1,9 @@
 @echo off 
 set /A request = "" 
 
-if %request% == git (
-  git.bat
-) else if %request% == web (
-  web.bat
-) else (
-  echo No Reference.
-)
+if %request% == "git" 
+git.bat
+else if %request% == web 
+web.bat
+else 
+echo No Reference.
